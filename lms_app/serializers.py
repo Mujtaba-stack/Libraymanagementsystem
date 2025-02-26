6bdb3b64ad765896a9ca21910256c0b4d7b4db8f
Sharjeel Sohail <shijisohail786@gmail.com>
2025-02-26 21:56:38 +0500

    Samjhaya hy

--- lms_app/serializers.py
+++ lms_app/serializers.py
@@ -1,33 +1,41 @@
 from rest_framework import serializers
-from .models import Member,Category,Book,Author,Reservation,Barrow
+from .models import Member, Category, Book, Author, Reservation, Barrow
+
 
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
-        fields= '__all__'
+        fields = '__all__'
+
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields='__all__'
+        fields = '__all__'
+
+
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+    # def create(self, validated_data):
+    #     Book.objects.create(**validated_data)
+
+
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = '__all__'
+
+
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = '__all__'
+
+
 class BarrowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Barrow
         fields = '__all__'
-                
-
-
-
-
-
