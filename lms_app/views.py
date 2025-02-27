91da5e8dbd32d89138452205783d944630b826a2
11784muj <115029208+11784muj@users.noreply.github.com>
2025-02-27 20:10:27 +0500

    Resolved merge conflicts in models.py and views.py

--- lms_app/views.py
+++ lms_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+<<<<<<< Updated upstream
 from django.db.models import Q
+=======
+>>>>>>> Stashed changes
 from rest_framework import status
 from .models import Member, Category, Book, Author, Reservation, Barrow
 from .serializers import (
@@ -69,6 +72,7 @@ def category_detail(request, id):
 
 @api_view(['GET', 'POST'])
 def book_list_create(request):
+<<<<<<< Updated upstream
     try:
         if request.method == 'GET':
             books = Book.objects.all()
@@ -83,6 +87,18 @@ def book_list_create(request):
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         print("Exception: ", e)
+=======
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+>>>>>>> Stashed changes
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def book_detail(request, id):
@@ -128,6 +144,7 @@ def author_detail(request, id):
     elif request.method == 'DELETE':
         author.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+<<<<<<< Updated upstream
 
 @api_view(['GET', 'POST'])
 def barrow_list_create(request):
@@ -206,5 +223,63 @@ def search_lms(request):
     return Response(response)
 
 
+=======
+>>>>>>> Stashed changes
 
+@api_view(['GET', 'POST'])
+def barrow_list_create(request):
+    if request.method == 'GET':
+        barrows = Barrow.objects.all()
+        serializer = BarrowSerializer(barrows, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = BarrowSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def barrow_detail(request, id):
+    barrow = get_object_or_404(Barrow, id=id)
+    if request.method == 'GET':
+        serializer = BarrowSerializer(barrow)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = BarrowSerializer(barrow, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        barrow.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def reservation_list_create(request):
+    if request.method == 'GET':
+        reservations = Reservation.objects.all()
+        serializer = ReservationSerializer(reservations, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ReservationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def reservation_detail(request, id):
+    reservation = get_object_or_404(Reservation, id=id)
+    if request.method == 'GET':
+        serializer = ReservationSerializer(reservation)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ReservationSerializer(reservation, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        reservation.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
